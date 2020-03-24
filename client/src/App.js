da46ff2b9acf0a608e5ae592a31ea128673606a3
Kay Zhao <sqkzhao@gmail.com>
2020-03-24 13:53:01 -0700

    updated movie details page; added overview component

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import { Router } from '@reach/router'
 import Home from './components/Home'
 import MovieDetails from './components/MovieDetails';
 import SearchTheaters from './components/SearchTheaters';
+import MovieDetailsOverview from './components/MovieDetailsOverview';
 
 function App() {
   return (
-    <div>
+    <div className="bg-dark text-white">
       <Router>
         <Home path='/' />
         <SearchTheaters path='/theaters' />
-        <MovieDetails path='/movies/:id' />
+        <MovieDetails path='/movies/:id/'>
+          <MovieDetailsOverview path='overview' />
+        </MovieDetails>
       </Router>
     </div>
   );

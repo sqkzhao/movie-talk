9ed575dbfe73281ed2293d667b1848266863b601
Kay Zhao <sqkzhao@gmail.com>
2020-03-25 20:09:28 -0700

    created chat box

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 
 require('./server/routes/user.routes')(app);
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log("Listening at Port 8000")
 });
+
+// SOCKET
+const io = require("socket.io")(server)
+const chat = []
+
+io.on("connection", socket => {
+    console.log("Socket established")
+    socket.emit("welcome", "welcome from the server")
+
+    // NOT WORKING
+    socket.on("new_msg", msg => {
+        console.log("received...")
+        chat.push(msg)
+        console.log(msg)
+        io.emit("chat_history", chat)
+    })
+
+
+})

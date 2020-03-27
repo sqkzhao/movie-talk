e691bc45feccdba6358681d575d6e6a1bea6be76
Kay Zhao <sqkzhao@gmail.com>
2020-03-26 20:50:36 -0700

    fixed link & aded search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router } from '@reach/router'
+import { Link, Router } from '@reach/router'
 import Home from './components/Home'
 import MovieDetails from './components/MovieDetails';
 import SearchTheaters from './components/SearchTheaters';
@@ -8,26 +8,26 @@ import MovieDetailsVideos from './components/MovieDetailsVideos';
 import MovieDetailsInfo from './components/MovieDetailsInfo';
 import MovieDetailsReview from './components/MovieDetailsReview';
 import Chat from './components/Chat'
+import Search from './components/Search'
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Search from './components/Search'
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
+import { Row, Col, Form } from 'react-bootstrap'
 
 function App() {
   return (
-
     <div style={{background:"rgb(255,193,7)"}}>
 
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">MOVIE TALK!</Navbar.Brand>
+        <Navbar.Brand><Link to="/"><span className="text-white"><i class="fas fa-film"></i><strong> MOVIE TALK!</strong></span></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link><Link to="/"><span className="text-light">Home</span></Link></Nav.Link>
             <NavDropdown title="Movies" id="collasible-nav-dropdown">
               <NavDropdown.Item href="/searchmovies#popular">Popular</NavDropdown.Item>
               <NavDropdown.Item href="/searchmovies#upcoming">Upcoming</NavDropdown.Item>
@@ -36,15 +36,20 @@ function App() {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="/sign_up">Sign Up</Nav.Link>
-            <Nav.Link href="/sign_in">Sign In</Nav.Link> 
+            <Nav.Link><Link to="/"><span className="text-light">My Profile</span></Link></Nav.Link>
+            <Nav.Link><Link to="/sign_up"><span className="text-light">Sign Up</span></Link></Nav.Link>
+            <Nav.Link><Link to="/sign_in"><span className="text-light">Sign In</span></Link></Nav.Link> 
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
+      <Row>
+        <Col>
+          <Form.Control type="text" placeholder="Search movies.." rounded-0 />
+        </Col>
+      </Row>
 
       <Router>
-
         <Home path='/' />
         <Search path='/searchmovies' />
         <SearchTheaters path='/theaters' />
@@ -60,6 +65,7 @@ function App() {
       </Router>
 
       <Chat />
+
     </div>
   );
 }

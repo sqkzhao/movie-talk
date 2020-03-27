3b76bd8e7c3c4455ec9064630a0110897ee935c1
Kay Zhao <sqkzhao@gmail.com>
2020-03-26 20:10:50 -0700

    working

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-
-const UserController = require("../controllers/user.controller")
-
-module.exports = app => {
-    app.get("/users", UserController.getAllUser)
-    app.post("/users", UserController.createUser)
-    app.get("/users/:id", UserController.getOneUser)
-    app.delete("/users/:id", UserController.deleteUser)
-    app.put("/users/:id", UserController.editUser)
-=======
 // const UserController = require("../controllers/user.controller")
 
 module.exports = function(app) {
@@ -17,5 +6,4 @@ module.exports = function(app) {
     // app.get("/users/:id", UserController.getOneUser)
     // app.delete("/users/:id", UserController.deleteUser)
     // app.put("/users/:id", UserController.editUser)
->>>>>>> 4725fda76dd1876f0f53cf3db12d07649b844455
 }

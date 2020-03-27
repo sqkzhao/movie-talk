4c8e033dc3f4a430550ef57504b4d2a41757481c
Kay Zhao <sqkzhao@gmail.com>
2020-03-26 19:28:02 -0700

    no message

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     firstName:{
         type: String,
+
         required: [true, "First name must be provided"],
         minlength: [3, "First name must be at least 3 characters"]
     },
@@ -20,8 +22,54 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "Password must be provided"],
         minlength: [3, "Password must be at least 3 characters"]
+
     }
+    validate: {
+        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+        message: "Please enter a valid email"
+      }
+      
+
 }, {timestamps:true})
+
+
+UserSchema.virtual('confirmPassword')
+  .get( () => this._confirmPassword )
+  .set( value => this._confirmPassword = value );
+
+
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this.confirmPassword) {
+      this.invalidate('confirmPassword', 'Password must match confirm password');
+    }
+    next();
+});
+
+
+
+ 
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 10)
+    .then(hash => {
+      this.password = hash;
+      next();
+});
+
+
+const bcrypt = require('bcrypt');
+
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 10)
+    .then(hash => {
+      this.password = hash;
+      next();
+    });
+});
+
+
+
+
 
 
 const User = mongoose.model("User", UserSchema)
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,3 +1,9 @@
+// const UserController = require("../controllers/user.controller")
+
 module.exports = function(app) {
-    
+    // app.get("/users", UserController.getAllUser)
+    // app.post("/users", UserController.createUser)
+    // app.get("/users/:id", UserController.getOneUser)
+    // app.delete("/users/:id", UserController.deleteUser)
+    // app.put("/users/:id", UserController.editUser)
 }
